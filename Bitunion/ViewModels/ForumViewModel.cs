61cec9378aeda53a531c28a3720c9237ffd1f0f9
Olivier <olenheim@hotmail.com>
2013-08-28 15:30:59 +0800

    创建论坛表单的view model

---
+++ Bitunion/ViewModels/ForumViewModel.cs
@@ -0,0 +1,111 @@
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+
+namespace Bitunion.ViewModels
+{   
+    public class ForumViewModel : INotifyPropertyChanged
+    {
+
+        public ForumViewModel(BitForum forum)
+        {
+            Name = Uri.UnescapeDataString(forum.name);
+            Description = Uri.UnescapeDataString(forum.description);
+            Moderator = Uri.UnescapeDataString(forum.moderator);
+            Onlines = forum.onlines;
+            this.forum= forum;
+        }
+
+        public ForumViewModel() { }
+
+        public BitForum forum { get; private set; }
+
+        //论坛名称
+        private string _name;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (value != _name)
+                {
+                    _name = value;
+                    NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        //论坛描述
+        private string _description;
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                if (value != _description)
+                {
+                    _description = value;
+                    NotifyPropertyChanged("Description");
+                }
+            }
+        }
+
+        //在线人数
+        private string _moderator;
+        public string Moderator
+        {
+            get
+            {
+                return _moderator;
+            }
+            set
+            {
+                if (value != _moderator)
+                {
+                    _moderator = value;
+                    NotifyPropertyChanged("Moderator");
+                }
+            }
+        }
+
+        //论坛描述
+        private string _onlines;
+        public string Onlines
+        {
+            get
+            {
+                return _onlines;
+            }
+            set
+            {
+                if (value != _onlines)
+                {
+                    _onlines = value;
+                    NotifyPropertyChanged("Onlines");
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (null != handler)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
